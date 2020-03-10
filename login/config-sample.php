932a3794bcbf13712c45dc9bd8fe3ec7b35b9de7
Bony_Chops <office.kentarou@gmail.com>
2020-03-10 23:55:49 +0900

    Init

---
+++ login/config-sample.php
@@ -0,0 +1,23 @@
+<?php
+/*---------------[重要！！]------------------------------------
+このconfig_sample.phpをconfig.phpへ名前を変更しないと動作しません！
+---------------------------------------------------------------
+*/
+//アプリケーションの Consumer Key と Consumer Secret
+$sTwitterConsumerKey = '***********************************'; //Consumer Key (API Key)
+$sTwitterConsumerSecret = '***********************************'; //Consumer Secret (API Secret)
+ 
+//アプリケーションのコールバックURL
+$sTwitterCallBackUri = 'https://**************************'; //コールバックURL
+
+//アクセストークンを保存するファイル名(変更推奨)
+$accesstoken_filename = "accesstoken.js";
+ 
+//変数初期化
+$objTwitterConection = NULL; //TwitterOAuthクラスのインスタンス化
+$aTwitterRequestToken = array(); //リクエストトークン
+$sTwitterRequestUrl = ''; //認証用URL
+$objTwitterAccessToken = NULL; //アクセストークン
+$objTwUserInfo = NULL; //ユーザー情報
+?>
+
---
+++ login/member.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+
+require_once 'config.php';
+require_once '../vendor/autoload.php';
+ 
+//インポート
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+if(empty($_SESSION['twAccessToken'])){
+ echo 'error access token!!';
+ exit;
+}
+ 
+$objTwitterConection = new TwitterOAuth
+ (
+ $sTwitterConsumerKey,
+ $sTwitterConsumerSecret,
+ $_SESSION['twAccessToken']['oauth_token'],
+ $_SESSION['twAccessToken']['oauth_token_secret']
+ );
+ 
+
+$objTwUserInfo = $objTwitterConection->get("account/verify_credentials");
+?>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>タイトル</title>
+<meta http-equiv="Content-Style-Type" content="text/css">
+</head>
+ 
+<h2>Twitter アカウント ログイン完了！</h2>
+<?php
+file_put_contents($accesstoken_filename,json_encode($_SESSION));
+?>
+アクセストークン、アクセストークンシークレットを保存しました。
+<?php echo $_SERVER['REQUEST_URI']; ?><br/>
+<a href="logout.php">ログアウト</a>
+ 
+<pre>
+<?php var_dump($_SESSION['twAccessToken']); ?>
+</pre>
+ 
+<pre>
+<?php var_dump($objTwUserInfo); ?>
+</pre>
+ 
+</body>
+</html>
