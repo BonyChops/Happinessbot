ff437120d240657d27f94e26b07b121bc2d046a4
Bony_Chops <office.kentarou@gmail.com>
2020-03-11 00:43:41 +0900

    upload

---
+++ task.php
@@ -0,0 +1,20 @@
+<?php
+ 
+//インクルード
+require_once 'config.php';
+require_once '../vendor/autoload.php';
+ 
+//インポート
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+$TwitterAccountInfo = json_decode($$accesstoken_filename,true);
+
+$objTwitterConection = new TwitterOAuth
+ (
+ $sTwitterConsumerKey,
+ $sTwitterConsumerSecret,
+ $TwitterAccountInfo['twAccessToken']['oauth_token'],
+ $TwitterAccountInfo['twAccessToken']['oauth_token_secret']
+ );
+
+ $objTwUserInfo = $objTwitterConection->post("status/update",["status" => "Hello, world!"]);
