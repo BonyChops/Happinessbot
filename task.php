07e6e511b0869187e442f391186703fad9a45cdb
Bony_Chops <office.kentarou@gmail.com>
2020-03-11 02:05:29 +0900

    TEST

--- task.php
+++ task.php
@@ -48,7 +48,7 @@ foreach($searchResult->{"statuses"} as $value){
         ($value->{"retweeted"} == false)){
             $no=0;
             for($i=0;$i<sizeof($minusword);$i++){
-                prinf($value->{"text"},$minusword[$i]."\n");
+                printf($value->{"text"},$minusword[$i]."\n");
                 if(strpos($value->{"text"},$minusword[$i]) != null){
                     $no = 1;
                     break;
