fe25caf5127a34eaa9038ca374499f55490c6e55
Bony_Chops <office.kentarou@gmail.com>
2020-03-11 02:16:27 +0900

    TEST

--- task.php
+++ task.php
@@ -22,22 +22,25 @@ var_dump($objTwUserInfo);
 $date = date('Y-m-d G:i:s');
 
 $minusword = [
-    "しね","死ね","最悪","ごみ","ゴミ","コロナ","フェミ","悪い","かす","カス","暴力","性被害","寝取","NTR","眠"
+    "しね","死ね","最悪","ごみ","ゴミ","コロナ","フェミ","悪い","かす","カス","暴力","性被害","寝取","NTR","クソ","だるい"
 ];
 
 $words = [
-    "おいし","美味し","いい天気","眠いな","買い物","好きな人","お腹いっぱい"
+    "おいし","美味し","いい天気","眠いな","買い物","好きな人","お腹いっぱい","腹減った","お腹へった","食べたい","おなかすいた","なつかしい"
 ];
-if(date('H') > 20){
+if((date('H') >= 20)||(date('H') <= 4)){
     $words = [
-        "眠いな","ねれない","寝れない","羊 数え","いい夢"
+        "眠いな","ねれない","寝れない","羊 数え","いい夢","腹減った","お腹へった","食べたい","おなかすいた"
+    ];
+}
+if((date('H') >= 5)||(date('H') <= 8)){
+    $words = [
+        "おはよう","まだ眠い","朝ごはん","太陽","いい気分"
     ];
 }
 
-
 srand(time());
 $cntWord = $words[rand(0,sizeof($words)-1)];
-$cntWord="眠い";
 printf($cntWord);
 $searchResult = $objTwitterConection->get("search/tweets",["q" => $cntWord, "count" => 100]);
 //var_dump($searchResult->{"statuses"}[0]);
@@ -48,7 +51,6 @@ foreach($searchResult->{"statuses"} as $value){
         ($value->{"retweeted"} == false)){
             $no=0;
             for($i=0;$i<sizeof($minusword);$i++){
-                printf(strpos($value->{"text"},$minusword[$i])."\n");
                 if((strpos($value->{"text"},$minusword[$i]) != null)||(strpos($value->{"text"},$minusword[$i]) == 0)){
                     $no = 1;
                     break;
