1a8f10e00a2ebf2b20acf6493075a2b161e24b4c
Bony_Chops <office.kentarou@gmail.com>
2020-03-12 23:39:34 +0900

    TEST

--- checkReply.php
+++ checkReply.php
@@ -34,7 +34,7 @@ if($json['minId'] == null){
 
 $cntMinId = -1;
 $idsStr = "";
-$searchResult = $objTwitterConection->get("search/tweets",["q" => '@'.$botname.' filter:replies', "count" => 100,"lang" => "ja", "result_type" => "recent"]);
+$searchResult = $objTwitterConection->get("search/tweets",["q" => $botname.' filter:replies', "count" => 100,"lang" => "ja", "result_type" => "recent"]);
 var_dump($searchResult);
 foreach($searchResult->{"statuses"} as $value){
     if($value->{"id"} > $minId){
