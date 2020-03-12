24140e77df3e6ccd4ff83472c7fa957ab0742819
Bony_Chops <office.kentarou@gmail.com>
2020-03-12 13:53:38 +0900

    Add checkLimit.php

---
+++ checkLimit.php
@@ -0,0 +1,25 @@
+<?php
+ 
+//インクルード
+require_once 'login/config.php';
+require_once 'vendor/autoload.php';
+ 
+//インポート
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+$TwitterAccountInfo = json_decode(file_get_contents('login/'.$accesstoken_filename),true);
+
+$objTwitterConection = new TwitterOAuth
+ (
+ $sTwitterConsumerKey,
+ $sTwitterConsumerSecret,
+ $TwitterAccountInfo['twAccessToken']['oauth_token'],
+ $TwitterAccountInfo['twAccessToken']['oauth_token_secret']
+ );
+
+ $objTwitterConection2 = new TwitterOAuth
+ (
+ $sTwitterConsumerKey,
+ $sTwitterConsumerSecret);
+
+ $result = $objTwitterConection2->get("application/rate_limit_status");
