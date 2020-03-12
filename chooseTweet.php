10495e401b021b5478f2bc9bb24ba2b1857f20e5
Bony_Chops <office.kentarou@gmail.com>
2020-03-12 14:03:45 +0900

    Edit choodeTweet

--- chooseTweet.php
+++ chooseTweet.php
@@ -71,7 +71,7 @@ function chooseTweet($objTwitterConection, $objTwitterConection2,$custom = "", $
                                     printf("Like!"."\n");
                                     $likeResult = $objTwitterConection->post("favorites/create",["id" => $value->{"id"}]);
                                 }
-                                return $str;
+                                return processTweet($str);
                             }
                         }
                 }
@@ -81,3 +81,52 @@ function chooseTweet($objTwitterConection, $objTwitterConection2,$custom = "", $
         }
     }
 }
+//From https://qiita.com/nobuyuki-ishii/items/2838e663e2ab8d99ffd5
+function processTweet($text) {
+    $text = deleteUser($text);
+    $text = deleteNewLine($text);
+    $text = deleteUrl($text);
+    //$text = deleteHashtag($text);
+    $text = deleteNonUtf8($text);
+    $text = convertHtmlSpecialCharcter($text);
+    return $text;
+  }
+  // ユーザ名（@～）を消す
+  function deleteUser($text) {
+    return preg_replace('/@.*\s/', '', $text);
+  }
+  
+  // 改行をスペースに変換
+  function deleteNewLine($text) {
+    return str_replace(array("\r\n", "\r", "\n"), ' ', $text);
+  }
+  
+  // http引用を消す
+  function deleteUrl($text) {
+    return preg_replace('/(https?)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)/', '', $text);
+  }
+  
+  // ハッシュタグ（#～）を消す
+  function deleteHashtag($text) {
+    return preg_replace('/#.*/', '', $text);
+  }
+  
+  // 変換不能文字を消す
+  function deleteNonUtf8($text) {
+    //reject overly long 2 byte sequences, as well as characters above U+10000 and replace with ?
+    $text = preg_replace(
+      '/[\x00-\x08\x10\x0B\x0C\x0E-\x19\x7F]'.
+      '|[\x00-\x7F][\x80-\xBF]+'.
+      '|([\xC0\xC1]|[\xF0-\xFF])[\x80-\xBF]*'.
+      '|[\xC2-\xDF]((?![\x80-\xBF])|[\x80-\xBF]{2,})'.
+      '|[\xE0-\xEF](([\x80-\xBF](?![\x80-\xBF]))|(?![\x80-\xBF]{2})|[\x80-\xBF]{3,})/S',
+      '', $text );
+  
+    //reject overly long 3 byte sequences and UTF-16 surrogates and replace with ?
+    $text = preg_replace(
+      '/\xE0[\x80-\x9F][\x80-\xBF]'.
+      '|\xED[\xA0-\xBF][\x80-\xBF]/S',
+      '', $text );
+  
+    return $text;
+  }
