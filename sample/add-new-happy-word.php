790977ce9a6d475787c3a28db809fa5b06855efe
Bony_Chops <office.kentarou@gmail.com>
2020-03-12 21:45:19 +0900

    TEST

--- sample/add-new-happy-word.php
+++ sample/add-new-happy-word.php
@@ -1,9 +1,46 @@
 <?php
 require_once('../login/config.php');
+$minusword = json_decode(file_get_contents("../words/negative.js",true));
+$words = json_decode(file_get_contents("../words/positive.js",true));
+ $others = json_decode(file_get_contents("../words/others.js",true));
+$nonrated = json_decode(file_get_contents("../words/nonrated.js",true)); 
+if($minusword == ""){
+    $minusword = array();
+}
+if($words == ""){
+    $words = array();
+}
+if($others == ""){
+    $others = array();
+}
+if($nonrated == ""){
+    $nonrated = array();
+}
 
-    var_dump($_POST);
-
-
+$chosei = 0;
+if(isset($_POST[0])){
+    for($i=0;$i<sizeof($nonrated);$i++){
+        if($_POST[$i] == "happy"){
+            array_push($words,$nonrated[$i-$chosei]);
+            array_splice($nonrated, $i-$chosei, 1);
+            $chosei--;
+        }
+        if($_POST[$i] == "minusword"){
+            array_push($minusword,$nonrated[$i-$chosei]);
+            array_splice($nonrated, $i-$chosei, 1);
+            $chosei--;
+        }
+        if($_POST[$i] == "others"){
+            array_push($others,$nonrated[$i-$chosei]);
+            array_splice($others, $i-$chosei, 1);
+            $chosei--;
+        }
+    }
+    file_put_contents("../words/negative.js",json_encode($minusword));
+    file_put_contents("../words/positive.js",json_encode($words));
+    file_put_contents("../words/others.js",json_encode($others));
+    file_put_contents("../words/nonrated.js",json_encode($nonrated));
+}
 
 if($_GET['passwd'] != $passwd){
 ?>
@@ -32,22 +69,7 @@ require_once('../../../header.php');
 
 <h2>Happy Tweets<span>幸せをさがそう</span></h2>
 <?php
-$minusword = json_decode(file_get_contents("../words/negative.js",true));
-$words = json_decode(file_get_contents("../words/positive.js",true));
- $others = json_decode(file_get_contents("../words/others.js",true));
-$nonrated = json_decode(file_get_contents("../words/nonrated.js",true)); 
-if($minusword == ""){
-    $minusword = array();
-}
-if($words == ""){
-    $words = array();
-}
-if($others == ""){
-    $others = array();
-}
-if($nonrated == ""){
-    $nonrated = array();
-}
+
 echo '<form method="post"><input type="submit" value="決定">';
 $cnt = 0;
 foreach($nonrated as $value){
