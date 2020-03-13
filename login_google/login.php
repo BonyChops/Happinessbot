f997e274416ae9dd666692615957d70b4944e49b
Bony_Chops <office.kentarou@gmail.com>
2020-03-13 22:21:48 +0900

    TEST

--- login_google/login.php
+++ login_google/login.php
@@ -9,4 +9,4 @@ $clientInfo = json_decode(file_get_contents("client_secret.json"));
             'response_type' => 'code',
     );
 
-    header('location: https://accounts.google.com/o/oauth2/auth?'). http_build_query($querys);
+    header('location: https://accounts.google.com/o/oauth2/auth?'. http_build_query($querys));
