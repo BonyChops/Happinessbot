a259e757ebd0d93380b392cea6859c2a84e7ea93
Bony_Chops <office.kentarou@gmail.com>
2020-03-13 23:54:12 +0900

    Add createAndUpload

---
+++ createAndUpload.php
@@ -0,0 +1,24 @@
+<?php
+$statuses = json_decode(file_get_contents("words/statuses.js",true));
+if($statuses == ""){
+    printf("There is no statuses.");
+    exit;
+}
+
+$targetStatus = $statuses[rand(0,sizeof($statuses)-1)];
+var_dump($targetStatus);
+
+//Run command to generate img and voice (in jp)
+printf("Generating img and voice...\n");
+exec('convert -font SourceHanSerif-Heavy.otf -gravity center -pointsize 100 -fill white -annotate 0 "'.$targetStatus->{"str"}.'" happy_back.png tmp.png & google_speech -l ja -o tmp.mp3 "'.$targetStatus->{"str"}.'"');
+printf("Volume up...\n");
+exec('mp3gain -g 9 tmp.mp3');
+printf("Mixing BGM and voice...\n");
+exec('ffmpeg -y -i back_bgm2.wav -i tmp.mp3 -filter_complex amix=inputs=2:duration=longest tmp2.mp3');
+printf("Rendering... (1 of 2)\n");
+exec('ffmpeg -y -r 60 -loop 1 -t 8.3 -i tmp.png -i tmp2.mp3 -vcodec libx264 -r 60 -ar 48000 tmp.mp4');
+printf("Rendering... (2 of 2)\n");
+//exec('ffmpeg -y -i video_back.mp4 -i tmp.mp4 -filter_complex "[0:v] [0:a] [1:v] [1:a] concat=n=2" output.mp4');
+exec('melt video_back.mp4 tmp.mp4 -consumer avformat:output.mp4 acodec=libmp3lame vcodec=libx264');
+
+require_once('uploadVideo.php');
--- uploadVideo.php
+++ uploadVideo.php
@@ -1,27 +1,11 @@
 <?php
-$statuses = json_decode(file_get_contents("words/statuses.js",true));
-if($statuses == ""){
-    printf("There is no statuses.");
-    exit;
+if(!isset($statuses)){
+    $statuses = json_decode(file_get_contents("words/statuses.js",true));
+    if($statuses == ""){
+        printf("There is no statuses.");
+        exit;
+    }
 }
-
-$targetStatus = $statuses[rand(0,sizeof($statuses)-1)];
-var_dump($targetStatus);
-
-//Run command to generate img and voice (in jp)
-printf("Generating img and voice...\n");
-exec('convert -font SourceHanSerif-Heavy.otf -gravity center -pointsize 100 -fill white -annotate 0 "'.$targetStatus->{"str"}.'" happy_back.png tmp.png & google_speech -l ja -o tmp.mp3 "'.$targetStatus->{"str"}.'"');
-printf("Volume up...\n");
-exec('mp3gain -g 9 tmp.mp3');
-printf("Mixing BGM and voice...\n");
-exec('ffmpeg -y -i back_bgm2.wav -i tmp.mp3 -filter_complex amix=inputs=2:duration=longest tmp2.mp3');
-printf("Rendering... (1 of 2)\n");
-exec('ffmpeg -y -r 60 -loop 1 -t 8.3 -i tmp.png -i tmp2.mp3 -vcodec libx264 -r 60 -ar 48000 tmp.mp4');
-printf("Rendering... (2 of 2)\n");
-//exec('ffmpeg -y -i video_back.mp4 -i tmp.mp4 -filter_complex "[0:v] [0:a] [1:v] [1:a] concat=n=2" output.mp4');
-exec('melt video_back.mp4 tmp.mp4 -consumer avformat:output.mp4 acodec=libmp3lame vcodec=libx264');
-
-
 
 /**
  * Library Requirements
@@ -92,7 +76,7 @@ if ($client->getAccessToken()) {
     // This example sets the video's title, description, keyword tags, and
     // video category.
     $snippet = new Google_Service_YouTube_VideoSnippet();
-    $time = json_decode(file_get_contents( __DIR__ . '/time.js'),true);
+    $time = json_decode(file_get_contents( __DIR__ . '/login_google/time.js'),true);
     if (isset( $time["time"])) {
         $time["time"]++;
         $timecnt=$time["time"];
