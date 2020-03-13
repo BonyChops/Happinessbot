8847cb238e2fe5a14e0669fa68dbd2241040c69e
Bony_Chops <office.kentarou@gmail.com>
2020-03-13 22:26:26 +0900

    TEST

--- login_google/callback.php
+++ login_google/callback.php
@@ -1,3 +1,32 @@
 <?php
 var_dump($_GET);
 var_dump($_POST);
+
+$clientInfo = json_decode(file_get_contents("client_secret.json"));
+
+$baseURL = 'https://accounts.google.com/o/oauth2/token';
+$params = array(
+        'code'          => $_GET['code'],
+        'client_id'     => $clientInfo->{"web"}->{"client_id"},
+        'client_secret' => $clientInfo->{"web"}->{"client_secret"},
+        'redirect_uri'  => $clientInfo->{"web"}->{"redirect_uris"}[0],
+        'grant_type'    => 'authorization_code'
+);
+$headers = array(
+        'Content-Type: application/x-www-form-urlencoded',
+);
+
+$options = array('http' => array(
+        'method' => 'POST',
+        'content' => http_build_query($params),
+        'header' => implode("\r\n", $headers),
+));
+
+$response = json_decode(
+        file_get_contents($baseURL, false, stream_context_create($options)));
+
+if(!$response || isset($response->error)){
+    printf("なしは草");
+}
+printf("アクセストークン！\n");
+printf($response->access_token);
