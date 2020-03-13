e793cd9d865f724de8465b13d58ffc38758e3433
Bony_Chops <office.kentarou@gmail.com>
2020-03-13 19:26:42 +0900

    Add sounds

--- uploadVideo.php
+++ uploadVideo.php
@@ -8,5 +8,5 @@ if($statuses == ""){
 $targetStatus = $statuses[rand(0,sizeof($statuses)-1)];
 //Run command to generate img and voice (in jp)
 exec('convert -font SourceHanSerif-Heavy.otf -gravity center -pointsize 100 -fill white -annotate 0 "'.$targetStatus['str'].'" happy_back.png tmp.png & google_speech -l ja -o tmp.mp3 "'.$targetStatus['str'].'"');
-exec('ffmpeg -r 60 -loop 1 -t 8.3 -i tmp.png -vcodec libx264 -pix_fmt yuv420p -r 60 tmp.mp4');
-exec('')
+exec('ffmpeg -y -r 60 -loop 1 -t 8.3 -i tmp.png -i tmp.mp3 -vcodec libx264 -pix_fmt yuv420p -r 60 tmp.mp4');
+exec('ffmpeg -y -i video_back.mp4 -i tmp.mp4 -filter_complex "concat=n=2" output.mp4');
