cf69d810b863d33dfac981c3974e3c97a7b28330
Bony_Chops <office.kentarou@gmail.com>
2020-03-13 20:30:27 +0900

    TEST

--- uploadVideo.php
+++ uploadVideo.php
@@ -8,12 +8,12 @@ if($statuses == ""){
 $targetStatus = $statuses[rand(0,sizeof($statuses)-1)];
 var_dump($targetStatus);
 //Run command to generate img and voice (in jp)
-printf("Generating img and voice...");
+printf("Generating img and voice...\n");
 exec('convert -font SourceHanSerif-Heavy.otf -gravity center -pointsize 100 -fill white -annotate 0 "'.$targetStatus->{"str"}.'" happy_back.png tmp.png & google_speech -l ja -o tmp.mp3 "'.$targetStatus->{"str"}.'"');
-printf("Mixing BGM and voice...");
+printf("Mixing BGM and voice...\n");
 exec('ffmpeg -y -i back_bgm2.wav -i tmp.mp3 -filter_complex amix=inputs=2:duration=longest tmp2.mp3');
-printf("Rendering... (1 of 2)");
+printf("Rendering... (1 of 2)\n");
 exec('ffmpeg -y -r 60 -loop 1 -t 8.3 -i tmp.png -i tmp2.mp3 -vcodec libx264 -r 60 -ar 48000 tmp.mp4');
-printf("Rendering... (2 of 2)");
+printf("Rendering... (2 of 2)\n");
 //exec('ffmpeg -y -i video_back.mp4 -i tmp.mp4 -filter_complex "[0:v] [0:a] [1:v] [1:a] concat=n=2" output.mp4');
 exec('melt video_back.mp4 tmp.mp4 -consumer avformat:output3.mp4 acodec=libmp3lame vcodec=libx264');
