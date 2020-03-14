893491aa8152cc67d73c95be1b04fe051bdbb609
Bony_Chops <office.kentarou@gmail.com>
2020-03-15 01:17:47 +0900

    TEST

--- sample/index.php
+++ sample/index.php
@@ -83,7 +83,7 @@ if(isset($_GET['search'])){
 <section>
 <h2>ソーシャルアカウント</h2>
 <p class="c">気軽にのぞいてね！</p>
-<p class="c"><a target="_blank" href="https://twitter.com/IamHappiestPoop" class="fab fa-twitter"></a></p>
+<p class="c"><a target="_blank" href="https://twitter.com/IamHappiestPoop" class="fab fa-twitter"></a>
 <a target="_blank" href="https://www.youtube.com/channel/UCtpBLAoNCGs32cnn8Mu06-A" class="fab fa-youtube"></a>
 <a target="_blank" href="discord.php" class="fab fa-discord"></a></p>
 </section>
