df72f8c436c872ef3580ee90b88e3848513e9907
Bony_Chops <office.kentarou@gmail.com>
2020-03-15 02:03:57 +0900

    Changed voice

--- google_assistant/callback.php
+++ google_assistant/callback.php
@@ -1,7 +1,6 @@
 <?php
 $arr = array(
-    'speech' => "どうもどうも！", 
-    'displayText' => "平素より大変お世話になっております。"
+    'fulfillment_text' => "平素より大変お世話になっております。"
 );
 header("Content-Type: application/json; charset=utf-8");
 echo json_encode($arr);
