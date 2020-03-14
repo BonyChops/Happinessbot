fa373c08a29997ed2a12b5fdea201f9be203ca42
Bony_Chops <office.kentarou@gmail.com>
2020-03-15 02:06:47 +0900

    Added

--- google_assistant/callback.php
+++ google_assistant/callback.php
@@ -1,6 +1,31 @@
 <?php
+require_once 'login/config.php';
+require_once 'vendor/autoload.php';
+require_once "../chooseTweet.php";
+//インポート
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+$TwitterAccountInfo = json_decode(file_get_contents('login/'.$accesstoken_filename),true);
+
+$objTwitterConection = new TwitterOAuth
+ (
+ $sTwitterConsumerKey,
+ $sTwitterConsumerSecret,
+ $TwitterAccountInfo['twAccessToken']['oauth_token'],
+ $TwitterAccountInfo['twAccessToken']['oauth_token_secret']
+ );
+
+ $objTwitterConection2 = new TwitterOAuth
+ (
+ $sTwitterConsumerKey,
+ $sTwitterConsumerSecret);
+
+ $str = chooseTweet($objTwitterConection,$objTwitterConection2,"",false);
 $arr = array(
-    'fulfillment_text' => "平素より大変お世話になっております。"
+    'fulfillment_text' => $str
 );
 header("Content-Type: application/json; charset=utf-8");
+
+
+
 echo json_encode($arr);
