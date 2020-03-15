4c3809f106e5577fe297d16a1ae4c8517e1afacb
Bony_Chops <office.kentarou@gmail.com>
2020-03-15 15:40:16 +0900

    Fix

--- LINE/callback.php
+++ LINE/callback.php
@@ -24,7 +24,7 @@ if($statuses == ""){
     $str = "ごめんなさい、今は無理です。。。";
 }else{
     $targetStatus = $statuses[rand(0,sizeof($statuses)-1)];
-    $str = "a";//$targetStatus->{"str"};
+    $str = $targetStatus->{"str"};
 }
 
 
