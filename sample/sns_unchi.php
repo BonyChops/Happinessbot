18adc6f13b4eb743ada04b87a0691b2d56ce4123
Bony_Chops <office.kentarou@gmail.com>
2020-03-16 21:30:26 +0900

    Edit sns_unchi.php

--- sample/sns_unchi.php
+++ sample/sns_unchi.php
@@ -1,12 +1,12 @@
 <section>
 <h2>ソーシャルアカウント</h2>
 <p class="c">気軽にのぞいてね！</p>
-<p class="c"><a target="_blank" href="https://bonychops.com/experiment/Happinessbot/sample/" class="fas fa-search"></a>
-<p class="c"><a target="_blank" href="https://twitter.com/IamHappiestPoop" class="fab fa-twitter"></a>
-<a target="_blank" href="https://www.youtube.com/channel/UCtpBLAoNCGs32cnn8Mu06-A" class="fab fa-youtube"></a>
-<a target="_blank" href="discord.php" class="fab fa-discord"></a>
-<a target="_blank" href="LINE.php" class="fab fa-line"></a>
-<a target="_blank" href="slack.php" class="fab fa-slack"></a></p>
+<p class="c"><a  href="https://bonychops.com/experiment/Happinessbot/sample/" class="fas fa-search"></a>
+<p class="c"><a  href="https://twitter.com/IamHappiestPoop" class="fab fa-twitter"></a>
+<a  href="https://www.youtube.com/channel/UCtpBLAoNCGs32cnn8Mu06-A" class="fab fa-youtube"></a>
+<a  href="discord.php" class="fab fa-discord"></a>
+<a  href="LINE.php" class="fab fa-line"></a>
+<a  href="slack.php" class="fab fa-slack"></a></p>
 </section>
 
 
